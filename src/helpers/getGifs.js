a52e646a22736a20498d2465593e96a4ec957b57
Valeria <valeria.alasia@tsoftglobal.com>
2025-07-01 11:13:27 -0300

    Seguridad

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,9 @@
+
+
 export const getGifs = async(category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=tn83KIHPT1tfNMVZUI1JGjnHlgPsSkf9&q=${category}&limit=5`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${import.meta.env.VITE_API_KEY}&q=${category}&limit=5`;
+    console.log(url)
     const resp = await fetch(url);
     const { data } = await resp.json();
     
